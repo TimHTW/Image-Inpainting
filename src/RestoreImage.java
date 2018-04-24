79346561b419d80eacdc15f6333f275a8d5b66ce
Tim <s76782@htw-dresden.de>
2018-04-24 15:43:03 +0200

    update upwind and change matrix reading

--- src/RestoreImage.java
+++ src/RestoreImage.java
@@ -21,57 +21,58 @@ public class RestoreImage {
 			e.printStackTrace();
 		}
 		
-		int width = img.getWidth();
-		int height = img.getHeight();
+		int rows = img.getHeight();
+		int columns = img.getWidth();
 		
+		System.out.println(rows+" "+columns);
 
-		double[][] I_redScreen = new double [width][height];
-		double[][] I_greenScreen = new double [width][height];
-		double[][] I_blueScreen = new double [width][height];
+		double[][] I_redScreen = new double [rows][columns];
+		double[][] I_greenScreen = new double [rows][columns];
+		double[][] I_blueScreen = new double [rows][columns];
 		
-		double[][] I_redScreen_ = new double [width][height];
-		double[][] I_greenScreen_ = new double [width][height];
-		double[][] I_blueScreen_ = new double [width][height];
+		double[][] I_redScreen_ = new double [rows][columns];
+		double[][] I_greenScreen_ = new double [rows][columns];
+		double[][] I_blueScreen_ = new double [rows][columns];
 		
-		double[][] I_redOld = new double [width][height];
-		double[][] I_greenOld = new double [width][height];
-		double[][] I_blueOld = new double [width][height];
+		double[][] I_redOld = new double [rows][columns];
+		double[][] I_greenOld = new double [rows][columns];
+		double[][] I_blueOld = new double [rows][columns];
 		
 		
 		
-		boolean[][] paintedArea = new boolean [width][height];
-		boolean[][] paintedArea_inner = new boolean[width][height];
-		boolean[][] borderPixel = new boolean [width][height];
+		boolean[][] paintedArea = new boolean [rows][columns];
+		boolean[][] paintedArea_inner = new boolean[rows][columns];
+		boolean[][] borderPixel = new boolean [rows][columns];
 		
 		boolean is_finish_inner;
 		boolean is_finish_outer;
 		
 
-		double [][] Wred = new double [width][height];
-		double [][] Wgreen = new double [width][height];
-		double [][] Wblue = new double [width][height];
-		double [][] Wred_ = new double [width][height];
-		double [][] Wgreen_ = new double [width][height];
-		double [][] Wblue_ = new double [width][height];
+		double [][] Wred = new double [rows][columns];
+		double [][] Wgreen = new double [rows][columns];
+		double [][] Wblue = new double [rows][columns];
+		double [][] Wred_ = new double [rows][columns];
+		double [][] Wgreen_ = new double [rows][columns];
+		double [][] Wblue_ = new double [rows][columns];
 		
 		
-		double [][] Ix_red = new double [width][height];
-		double [][] Iy_red = new double [width][height];
-		double [][] Ix_green = new double [width][height];
-		double [][] Iy_green = new double [width][height];
-		double [][] Ix_blue = new double [width][height];
-		double [][] Iy_blue = new double [width][height];
-		double [][] Ix_rgb = new double [width][height];
-		double [][] Iy_rgb = new double [width][height];
+		double [][] Ix_red = new double [rows][columns];
+		double [][] Iy_red = new double [rows][columns];
+		double [][] Ix_green = new double [rows][columns];
+		double [][] Iy_green = new double [rows][columns];
+		double [][] Ix_blue = new double [rows][columns];
+		double [][] Iy_blue = new double [rows][columns];
+		double [][] Ix_rgb = new double [rows][columns];
+		double [][] Iy_rgb = new double [rows][columns];
 		
-		double [][] Xscalar_red = new double [width][height];
-		double [][] Xscalar_green = new double [width][height];
-		double [][] Xscalar_blue = new double [width][height];
+		double [][] Xscalar_red = new double [rows][columns];
+		double [][] Xscalar_green = new double [rows][columns];
+		double [][] Xscalar_blue = new double [rows][columns];
 		
 		
-		double [][] Yscalar_red = new double [width][height];
-		double [][] Yscalar_green = new double [width][height];
-		double [][] Yscalar_blue = new double [width][height];
+		double [][] Yscalar_red = new double [rows][columns];
+		double [][] Yscalar_green = new double [rows][columns];
+		double [][] Yscalar_blue = new double [rows][columns];
 		
 		
 		double t = 0;
@@ -83,15 +84,14 @@ public class RestoreImage {
 		/*--END declaration zone--*/
 		
 		/*--START Fill Matrices with values--*/
-		for(int i=0; i<width; i++) {
-			for(int j=0; j<height; j++) {
-
-				Color c = new Color(img.getRGB(i,j));
+		for(int i=0; i<img.getHeight(); i++) {
+			for(int j=0; j<img.getWidth(); j++) {
+				
+				Color c = new Color(img.getRGB(j,i));
 				I_redScreen[i][j] = c.getRed();
 				I_greenScreen[i][j] = c.getGreen();
 				I_blueScreen[i][j] = c.getBlue();
-				
-				borderPixel[i][j] = false;
+
 				if (I_redScreen[i][j] == 255 && I_greenScreen[i][j] == 0 && I_blueScreen[i][j] == 0) {
 					
 					paintedArea[i][j] = true;
@@ -102,6 +102,13 @@ public class RestoreImage {
 					borderPixel[i][j+1] = true;
 					borderPixel[i][j-1] = true;
 					
+					
+				}
+			}
+		}
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
+				if (I_redScreen[i][j] == 255 && I_greenScreen[i][j] == 0 && I_blueScreen[i][j] == 0) {
 					borderPixel[i][j]	= false;
 				}
 			}
@@ -112,8 +119,8 @@ public class RestoreImage {
 		I_blueScreen_=I_blueScreen;
 		
 		/*--START first value for W--*/
-		for(int i=1; i<width-1; i++) {
-			for(int j=1; j<height-1; j++) {
+		for(int i=1; i<rows-1; i++) {
+			for(int j=1; j<columns-1; j++) {
 				
 				Wred[i][j] = I_redScreen[i-1][j] - 4*I_redScreen[i][j] + I_redScreen[i+1][j] + I_redScreen[i][j-1] + I_redScreen[i][j+1];
 				Wgreen[i][j] = I_greenScreen[i-1][j] - 4*I_greenScreen[i][j] + I_greenScreen[i+1][j] + I_greenScreen[i][j-1] + I_greenScreen[i][j+1];
@@ -128,8 +135,8 @@ public class RestoreImage {
 		/*--START Euler outer loop--*/
 		outer_loop: {
 			while(t<50) {
-				for(int i=0; i<width; i++) {
-					for(int j=0; j<height; j++) {
+				for(int i=0; i<rows; i++) {
+					for(int j=0; j<columns; j++) {
 						if(paintedArea[i][j]) {
 							
 							I_redOld[i][j] = I_redScreen[i][j];
@@ -137,24 +144,24 @@ public class RestoreImage {
 							I_blueOld[i][j] = I_blueScreen[i][j];
 							
 							/*--START Upwind--*/
-							Iy_red[i][j] = (I_redScreen[i][j-1] - I_redScreen[i][j+1]) / 2.0;
-							Ix_red[i][j] = -(I_redScreen[i+1][j] - I_redScreen[i-1][j]) / 2.0;
-							Iy_green[i][j] = (I_greenScreen[i][j-1] - I_greenScreen[i][j+1]) / 2.0;
-							Ix_green[i][j] = -(I_greenScreen[i+1][j] - I_greenScreen[i-1][j]) / 2.0;
-							Iy_blue[i][j] = (I_blueScreen[i][j-1] - I_blueScreen[i][j+1]) / 2.0;
-							Ix_blue[i][j] = -(I_blueScreen[i+1][j] - I_blueScreen[i-1][j]) / 2.0;
+							Iy_red[i][j] = (I_redScreen[i-1][j] - I_redScreen[i+1][j]) / 2.0;
+							Ix_red[i][j] = -(I_redScreen[i][j+1] - I_redScreen[i][j-1]) / 2.0;
+							Iy_green[i][j] = (I_greenScreen[i-1][j] - I_greenScreen[i+1][j]) / 2.0;
+							Ix_green[i][j] = -(I_greenScreen[i][j+1] - I_greenScreen[i][j-1]) / 2.0;
+							Iy_blue[i][j] = (I_blueScreen[i-1][j] - I_blueScreen[i+1][j]) / 2.0;
+							Ix_blue[i][j] = -(I_blueScreen[i][j+1] - I_blueScreen[i][j-1]) / 2.0;
 							
 							Ix_rgb[i][j] = (Ix_red[i][j] + Ix_green[i][j] + Ix_blue[i][j]) / 3.0;
 							Iy_rgb[i][j] = (Iy_red[i][j] + Iy_green[i][j] + Iy_blue[i][j]) / 3.0;
 								
 							if (Ix_rgb[i][j] > 0) {
-								Xscalar_red[i][j] = Ix_rgb[i][j] * ( (Wred[i][j]-Wred[i-1][j]) /2.0 );
-								Xscalar_green[i][j] = Ix_rgb[i][j] * ( (Wgreen[i][j]-Wgreen[i-1][j]) /2.0 );
-								Xscalar_blue[i][j] = Ix_rgb[i][j] * ( (Wblue[i][j]-Wblue[i-1][j]) /2.0 );
+								Xscalar_red[i][j] = Ix_rgb[i][j] * ( (Wred[i][j]-Wred[i+1][j]) /2.0 );
+								Xscalar_green[i][j] = Ix_rgb[i][j] * ( (Wgreen[i][j]-Wgreen[i+1][j]) /2.0 );
+								Xscalar_blue[i][j] = Ix_rgb[i][j] * ( (Wblue[i][j]-Wblue[i+1][j]) /2.0 );
 							} else if (Ix_rgb[i][j] < 0){
-								Xscalar_red[i][j] = Ix_rgb[i][j] * ( (Wred[i+1][j]-Wred[i][j]) /2.0 );
-								Xscalar_green[i][j] = Ix_rgb[i][j] * ( (Wgreen[i+1][j]-Wgreen[i][j]) /2.0 );
-								Xscalar_blue[i][j] = Ix_rgb[i][j] * ( (Wblue[i+1][j]-Wblue[i][j]) /2.0 );
+								Xscalar_red[i][j] = Ix_rgb[i][j] * ( (Wred[i-1][j]-Wred[i][j]) /2.0 );
+								Xscalar_green[i][j] = Ix_rgb[i][j] * ( (Wgreen[i-1][j]-Wgreen[i][j]) /2.0 );
+								Xscalar_blue[i][j] = Ix_rgb[i][j] * ( (Wblue[i-1][j]-Wblue[i][j]) /2.0 );
 							}
 							
 							if (Iy_rgb[i][j] > 0) {
@@ -173,9 +180,9 @@ public class RestoreImage {
 							double scalar_blue = Xscalar_blue[i][j] + Yscalar_blue[i][j];
 							/*--END Upwind--*/
 							
-	//						double scalar_red = ( (-(I_redScreen[i+1][j]-I_redScreen[i-1][j]) / 2)  * ((Wred[i][j+1]-Wred[i][j-1]) / 2) ) + ( ((I_redScreen[i][j+1]-I_redScreen[i][j-1]) / 2) * ((Wred[i+1][j]-Wred[i-1][j]) / 2) );
-	//						double scalar_green = ( (-(I_greenScreen[i+1][j]-I_greenScreen[i-1][j]) / 2)  * ((Wgreen[i][j+1]-Wgreen[i][j-1]) / 2) ) + ( ((I_greenScreen[i][j+1]-I_greenScreen[i][j-1]) / 2) * ((Wgreen[i+1][j]-Wgreen[i-1][j]) / 2) );
-	//						double scalar_blue = ( (-(I_blueScreen[i+1][j]-I_blueScreen[i-1][j]) / 2)  * ((Wblue[i][j+1]-Wblue[i][j-1]) / 2) ) + ( ((I_blueScreen[i][j+1]-I_blueScreen[i][j-1]) / 2) * ((Wblue[i+1][j]-Wblue[i-1][j]) / 2) );
+//							double scalar_red = ( (-(I_redScreen[i+1][j]-I_redScreen[i-1][j]) / 2)  * ((Wred[i][j+1]-Wred[i][j-1]) / 2) ) + ( ((I_redScreen[i][j+1]-I_redScreen[i][j-1]) / 2) * ((Wred[i+1][j]-Wred[i-1][j]) / 2) );
+//							double scalar_green = ( (-(I_greenScreen[i+1][j]-I_greenScreen[i-1][j]) / 2)  * ((Wgreen[i][j+1]-Wgreen[i][j-1]) / 2) ) + ( ((I_greenScreen[i][j+1]-I_greenScreen[i][j-1]) / 2) * ((Wgreen[i+1][j]-Wgreen[i-1][j]) / 2) );
+//							double scalar_blue = ( (-(I_blueScreen[i+1][j]-I_blueScreen[i-1][j]) / 2)  * ((Wblue[i][j+1]-Wblue[i][j-1]) / 2) ) + ( ((I_blueScreen[i][j+1]-I_blueScreen[i][j-1]) / 2) * ((Wblue[i+1][j]-Wblue[i-1][j]) / 2) );
 							
 							//!!!!Wir beziehen hier die Pixel j+1, i+1 in die Berechnung ein, obwohl diese Werte noch gar nicht berechnet wurden 
 							
@@ -196,8 +203,8 @@ public class RestoreImage {
 				inner_loop: {
 					while(z<50) {
 						
-						for(int m=0; m<width; m++) {
-							for(int n=0; n<height; n++) {
+						for(int m=0; m<rows; m++) {
+							for(int n=0; n<columns; n++) {
 								if(paintedArea_inner[m][n]) {
 									
 									double redOld = I_redScreen[m][n];
@@ -208,26 +215,26 @@ public class RestoreImage {
 									I_greenScreen_[m][n] = tau2*(alpha*( (I_greenScreen[m][n-1] - 4*I_greenScreen[m][n] + I_greenScreen[m][n+1] + I_greenScreen[m-1][n] + I_greenScreen[m+1][n]) - Wgreen[m][n])) + greenOld;
 									I_blueScreen_[m][n] = tau2*(alpha*( (I_blueScreen[m][n-1] - 4*I_blueScreen[m][n] + I_blueScreen[m][n+1] + I_blueScreen[m-1][n] + I_blueScreen[m+1][n]) - Wblue[m][n])) + blueOld;
 									
-									if ( Math.abs(redOld - I_redScreen_[m][n]) <= 0.001 && Math.abs(greenOld - I_greenScreen_[m][n]) <= 0.001 && Math.abs(blueOld - I_blueScreen_[m][n]) <= 0.001 ) {
-										paintedArea_inner[m][n] = false;
-									}
+//									if ( Math.abs(redOld - I_redScreen_[m][n]) <= 0.001 && Math.abs(greenOld - I_greenScreen_[m][n]) <= 0.001 && Math.abs(blueOld - I_blueScreen_[m][n]) <= 0.001 ) {
+//										paintedArea_inner[m][n] = false;
+//									}
 									
-									if(m == 213 && n == 161) {
+									if(m == 96 && n == 153) {
 										logfile.println(Math.abs(greenOld - I_greenScreen[m][n]));
 									}
 									
-									is_finish_inner = true;
-									inner_break_loop: {
-										for(int a=0; a<width; a++) {
-											for(int b=0;b<height;b++) {
-												if(paintedArea_inner[a][b]) {
-													is_finish_inner = false;
-													break inner_break_loop;
-												}
-											}
-										}
-									}
-									if(is_finish_inner) break inner_loop;
+//									is_finish_inner = true;
+//									inner_break_loop: {
+//										for(int a=0; a<rows; a++) {
+//											for(int b=0;b<columns;b++) {
+//												if(paintedArea_inner[a][b]) {
+//													is_finish_inner = false;
+//													break inner_break_loop;
+//												}
+//											}
+//										}
+//									}
+//									if(is_finish_inner) break inner_loop;
 									
 								}
 							}
@@ -242,37 +249,38 @@ public class RestoreImage {
 				/*--END Euler inner loop--*/
 				
 				/*--START Check if values converge in outer loop--*/
-				for(int i=0; i<width; i++) {
-					for(int j=0; j<height; j++) {					
+				for(int i=0; i<rows; i++) {
+					for(int j=0; j<columns; j++) {					
 						if(paintedArea[i][j]) {
 	
-							if ( Math.abs(I_redOld[i][j] - I_redScreen[i][j]) <= 0.001 && Math.abs(I_greenOld[i][j] - I_greenScreen[i][j]) <= 0.001 && Math.abs(I_blueOld[i][j] - I_blueScreen[i][j]) <= 0.001 ) {
-								paintedArea[i][j] = false;
-							}
+//							if ( Math.abs(I_redOld[i][j] - I_redScreen[i][j]) <= 0.001 && Math.abs(I_greenOld[i][j] - I_greenScreen[i][j]) <= 0.001 && Math.abs(I_blueOld[i][j] - I_blueScreen[i][j]) <= 0.001 ) {
+//								paintedArea[i][j] = false;
+//							}
 							
-							is_finish_outer = true;
-							outer_break_loop: {
-								for(int a=0; a<width; a++) {
-									for(int b=0;b<height;b++) {
-										if(paintedArea[a][b]) {
-											is_finish_outer = false;
-											break outer_break_loop;
-										}
-									}
-								}
-							}
-							if(is_finish_outer) break outer_loop;
+//							is_finish_outer = true;
+//							outer_break_loop: {
+//								for(int a=0; a<rows; a++) {
+//									for(int b=0;b<columns;b++) {
+//										if(paintedArea[a][b]) {
+//											is_finish_outer = false;
+//											break outer_break_loop;
+//										}
+//									}
+//								}
+//							}
+//							if(is_finish_outer) break outer_loop;
 							
 						}
 					}
 				}
 				/*--END Check if values converge in outer loop--*/
 				
-				
+				int counter_border = 0;
 				/*--START W on border--*/
-				for(int i=0; i<width; i++) {
-					for(int j=0; j<height; j++) {
+				for(int i=0; i<rows; i++) {
+					for(int j=0; j<columns; j++) {
 						if(borderPixel[i][j]) {
+							counter_border++;
 							
 							Wred[i][j] = I_redScreen[i-1][j] - 4*I_redScreen[i][j] + I_redScreen[i+1][j] + I_redScreen[i][j-1] + I_redScreen[i][j+1];
 							Wgreen[i][j] = I_greenScreen[i-1][j] - 4*I_greenScreen[i][j] + I_greenScreen[i+1][j] + I_greenScreen[i][j-1] + I_greenScreen[i][j+1];
@@ -282,6 +290,8 @@ public class RestoreImage {
 				}
 				/*--END W on border--*/
 				t = t + tau;
+
+				System.out.println(counter_border);
 			}
 		}
 		/*--END Euler outer loop--*/
@@ -289,8 +299,8 @@ public class RestoreImage {
 		logfile.close();
 		
 		/*--START put screens together to create result image--*/
-		for(int i=0; i<width; i++) {
-			for(int j=0; j<height; j++) {
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
 				
 				int r = (int)I_redScreen[i][j];
 				int g = (int)I_greenScreen[i][j];
@@ -305,7 +315,7 @@ public class RestoreImage {
 				if (b > 255) b=255;
 			
 				Color newColor = new Color(r,g,b);
-				resultImg.setRGB(i, j, newColor.getRGB());
+				resultImg.setRGB(j, i, newColor.getRGB());
 			}
 		}
 		try {
